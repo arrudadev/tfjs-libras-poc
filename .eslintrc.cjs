0c63892b1e7ef87b4d0aa947297ced66e2c5d0ac
Alexandre Arruda <alexandre.monteiro.bec@gmail.com>
2024-01-14 08:25:46 -0300

    add rockeseat eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,8 +5,9 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    '@rocketseat/eslint-config/react',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
